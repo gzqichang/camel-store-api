a213f97e42fa65167535a678c82310dddb36df1d
赖勇浩 <lyh@gzqichang.com>
2020-02-14 00:27:44 +0800

    fix: 修复SearchGoods的权限问题。

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from rest_framework import viewsets, mixins
 from rest_framework.views import APIView, status
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from quser.permissions import CURDPermissionsOrReadOnly
 from wxapp.permissions import OnlyWxUser
@@ -223,7 +223,7 @@ class Search(APIView):
 
 
 class SearchGoods(APIView):
-    permission_classes = [CURDPermissionsOrReadOnly, ]
+    permission_classes = [IsAdminUser, ]
     serializer_class = SearchGoodsSerializer
 
     def get(self, request, *args, **kwargs):
